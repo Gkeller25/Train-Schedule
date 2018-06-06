ec110272eb50e5e0583a59e42e977662ff00d5b6
Garet <kellergaret@outlook.com>
2018-06-06 09:28:07 -0600

    finished train schedule

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Train-Schedule
+
+This is a Star Wars themed Train Schedule created by a combination of Javascript and Jquery using Bootstrap and custom Css.
+
+The user upon looking will see trains with arrival times that will update dynamically every minute and display how long until the train will arrive. User is also able to add additional trains and designate the time that the first train starts it's schedule and how frequently it runs.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,7 +9,7 @@
   firebase.initializeApp(config);
 
   var database = firebase.database();
-
+  
 
   $("#add-train-btn").on("click", function(event){
     event.preventDefault();
@@ -17,7 +17,6 @@
     var dest = $("#destination-input").val().trim();
     var trainFirst = $("#start-input").val().trim();
     var freq = $("#rate-input").val().trim();
-
     var newTrain = {
         train: name,
         destination: dest,
@@ -26,43 +25,32 @@
   
   
       };
-
+      if (name === ""){
+        alert('you did not fill out one of the fields');
+        return false;
+      } else if(dest === "") {
+        alert('you did not fill out one of the fields');
+        return false;
+      } else if(trainFirst === ""){
+        alert('you did not fill out one of the fields');
+        return false;
+      } else if (freq === ""){
+        alert('you did not fill out one of the fields');
+        return false;
+      } else {
       
 
-    database.ref().push(newTrain);
-
-    console.log(newTrain.train);
-    console.log(newTrain.destination);
-    console.log(newTrain.firstTime);
-    console.log(newTrain.frequency);
-
-    var trainFirstConverted = moment(trainFirst, "HH:mm").subtract(1, "years");
-    console.log(trainFirstConverted); 
-
-
+        database.ref().push(newTrain);
+      }
 
     $("#train-name-input").val("");
     $("#destination-input").val("");
     $("#start-input").val("");
     $("#rate-input").val("");
-
-    var currentTime = moment();
-    console.log(currentTime);
-    console.log("CURRENT TIME: " + moment(trainFirstConverted).format("hh:mm"));
-var diffTime = moment().diff(moment(trainFirstConverted),"minutes");
-console.log("DIFFERENCE IN TIME:  " + diffTime);
-var timeRemain = diffTime % freq;
-console.log(timeRemain);
- var timeMinTillTrain = freq - timeRemain;
- console.log("MINUTES TILL TRAIN:  " + timeMinTillTrain);
- var nextTrain = moment().add(timeMinTillTrain, "minutes");
- console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
   })
 
 
   database.ref().on("child_added", function(childSnapshot, prevChildKey) {
-
-    console.log(childSnapshot.val());
 
     var name = childSnapshot.val().train;
     var dest = childSnapshot.val().destination;
@@ -71,33 +59,37 @@ console.log(timeRemain);
 
 
     var trainFirstConverted = moment(trainFirst, "hh:mm").subtract(1, "years");
-    console.log(trainFirstConverted); 
-
     var currentTime = moment();
-    console.log(currentTime);
-    console.log("CURRENT TIME: " + moment(trainFirstConverted).format("hh:mm"));
-var diffTime = moment().diff(moment(trainFirstConverted),"minutes");
-console.log("DIFFERENCE IN TIME:  " + diffTime);
-var timeRemain = diffTime % freq;
-console.log(timeRemain);
- var timeMinTillTrain = freq - timeRemain;
- console.log("MINUTES TILL TRAIN:  " + timeMinTillTrain);
- var nextTrain = moment().add(timeMinTillTrain, "minutes");
-var arrivalTime = moment(nextTrain).format("hh:mm");
+    var diffTime = moment().diff(moment(trainFirstConverted),"minutes");
+    var timeRemain = diffTime % freq;
+    var timeMinTillTrain = freq - timeRemain;
+    var nextTrain = moment().add(timeMinTillTrain, "minutes");
+    var arrivalTime = moment(nextTrain).format("hh:mm A");
+    var trainFirstPretty = moment.unix(trainFirst).format("HH:mm A");
+    $("#train-table > tbody").append("<tr id='"+childSnapshot.key+"'><td>" + name + "</td><td>" + dest + "</td><td>" +
+    freq + "</td><td>" + arrivalTime + "</td><td>" + timeMinTillTrain + "</td></tr>");
+    
+    var timer = setInterval(tableUpdate, moment.duration(1, "minutes"));
+
+    function tableUpdate(){
+       
+      var trainFirstConverted = moment(trainFirst, "hh:mm A").subtract(1, "years");
+      var currentTime = moment();
+      var diffTime = moment().diff(moment(trainFirstConverted),"minutes");
+      var timeRemain = diffTime % freq;
+      var timeMinTillTrain = freq - timeRemain;
+      var nextTrain = moment().add(timeMinTillTrain, "minutes");
+      var arrivalTime = moment(nextTrain).format("hh:mm A");
+      var trainFirstPretty = moment.unix(trainFirst).format("HH:mm A");
+
+        
+        $("#"+childSnapshot.key).replaceWith("<tr id='"+childSnapshot.key+"'><td>" + name + "</td><td>" + dest + "</td><td>" +
+    freq + "</td><td>" + arrivalTime + "</td><td>" + timeMinTillTrain + "</td></tr>");
+    }  
+})
 
 
 
-  console.log(name);
-  console.log(dest);
-  console.log(trainFirst);
-  console.log(freq);
-
-    var trainFirstPretty = moment.unix(trainFirst).format("HH:mm");
-
-
-    $("#train-table > tbody").append("<tr><td>" + name + "</td><td>" + dest + "</td><td>" +
-    freq + "</td><td>" + arrivalTime + "</td><td>" + timeMinTillTrain + "</td><td>" + "" + "</td></tr>");
-
-  })
-
+    
    
+  
